50155d8d7d6567aaef9b899f7d4f23cfab96ff43
uzzielsr <54377537+uzzielsr@users.noreply.github.com>
2025-06-21 10:57:50 -0400

    Latest branch

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -4,7 +4,6 @@ import { LoginPage } from '../pages/login.page';
 test.describe('Login Suite', () => {
   test('User can log in with valid credentials', async ({ page }) => {
     const loginPage = new LoginPage(page);
-
     await loginPage.goto();
     await loginPage.login('user@example.com', 'password123');
     await expect(page).toHaveURL(/.*dashboard/);
