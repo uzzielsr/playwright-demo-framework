4ac29a5736b3fa7dfa00af459b1b77879861bb5f
uzzielsr <54377537+uzzielsr@users.noreply.github.com>
2025-07-13 17:17:16 -0400

    Add zealous-techtestrail-reporter

---
+++ testrail.config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+module.exports = {
+  base_url: process.env.TESTRAIL_HOST,
+  user: process.env.TESTRAIL_USER,
+  pass: process.env.TESTRAIL_PASSWORD,
+  project_id: Number(process.env.TESTRAIL_PROJECT_ID),
+  suite_id: Number(process.env.TESTRAIL_SUITE_ID),
+  create_missing_cases: true,
+  testRailUpdateInterval: 0,
+  updateResultAfterEachCase: true,
+  use_existing_run: {
+    id: 0,
+  },
+  create_new_run: {
+    include_all: false,
+    run_name: "Test Run",
+    milestone_id: 0,
+  },
+  status: {
+    passed: 1,
+    failed: 5,
+    untested: 3,
+    skipped: 6,
+  },
+};
